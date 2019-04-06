94864258245d58f5b2c83e351c8fb91e905c6539
Gabriel Zigue <gzigue@yahoo.com.br>
2019-04-06 00:37:30 -0300

    teste de build em produção

--- src/API.js
+++ src/API.js
@@ -2,7 +2,7 @@ const dev = "http://localhost:8080/";
 const prod = "https://balistic.herokuapp.com/";
 
 //Mudar aqui para trocar o ambiente. "dev" para desenvolvimento, "prod" para produção
-const environment = dev;
+const environment = prod;
 
 const API_CONFIG = {
     address: environment,
